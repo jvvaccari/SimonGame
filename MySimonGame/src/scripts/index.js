5f35ba6ec6648cbb51730e4aef3080de038ea246
joao <joao.vaccari@majovdev.com.br>
2024-06-26 16:07:09 -0300

    project structure

---
+++ MySimonGame/src/scripts/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT,()=>{
+    console.log(`The express is running on port: ${PORT}`);
+});
+
+app.get('/',(req,res)=>{
+    res.send('');
+});
